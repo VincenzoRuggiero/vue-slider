0030bb4fd9f0047ba301639ae31b16e9c73a9c23
VincenzoRuggiero <vincenzoru@live.it>
2022-12-13 15:30:52 +0100

    Slider's title and text content changes dynamically with the picture shown

--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
 const { createApp } = Vue;
 
-// const slides =  [
-//             {
-//             image: "img/01.webp",
-//             title: "Marvel's Spiderman Miles Morale",
-//             text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-//           },
-//           {
-//             image: "img/02.webp",
-//             title: "Ratchet & Clank: Rift Apart",
-//             text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-//           },
-//           {
-//             image: "img/03.webp",
-//             title: "Fortnite",
-//             text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//           },
-//           {
-//             image: "img/04.webp",
-//             title: "Stray",
-//             text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-//           },
-//           {
-//             image: "img/05.webp",
-//             title: "Marvel's Avengers",
-//             text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-//           },
-//         ],
-
 createApp({
   data() {
     return {
-      activeImg: 0,
-      imagesList: [
+      activeIndex: 0,
+      objectsList: [
         {
           image: "img/01.webp",
           title: "Marvel's Spiderman Miles Morale",
@@ -64,16 +36,16 @@ createApp({
 
   methods: {
     prevImg() {
-      this.activeImg--;
-      if (this.activeImg < 0) {
-        this.activeImg = this.imagesList.length - 1;
+      this.activeIndex--;
+      if (this.activeIndex < 0) {
+        this.activeIndex = this.objectsList.length - 1;
       }
     },
 
     nextImg() {
-      this.activeImg++;
-      if (this.activeImg >= this.imagesList.length) {
-        this.activeImg = 0;
+      this.activeIndex++;
+      if (this.activeIndex >= this.objectsList.length) {
+        this.activeIndex = 0;
       }
     },
   },
